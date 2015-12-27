21d3126f0a5eb87523659c13488f52473378ac04
zbing0 <1587124918@qq.com>
2015-12-28 01:50:28 +0800

    json

--- src/com/example/presenter/ActManage.java
+++ src/com/example/presenter/ActManage.java
@@ -3,12 +3,12 @@ package com.example.presenter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import com.example.model.Activity;
 import com.example.model.User;
 import com.example.util.ConnectUtil;
@@ -61,8 +61,27 @@ public class ActManage {
 		Map<String,String> map = new HashMap<String,String>();
 		map.put("type", "26");
 		String resp = ConnectUtil.getResponse(map);
-		if(!resp.equals("false"))
+		if(!resp.equals("false")){
 			activitys = JSON.parseArray(resp,Activity.class);
+			
+//			try {
+//				JSONObject result = new JSONObject(resp);
+//				JSONArray actList = result.getJSONArray("actList");//获取JSONArray  
+//				int size=actList.length();
+//				for(int i=0;i<size;i++){
+//					JSONObject oj = actList.getJSONObject(i);
+//					Activity a=new Activity();
+//					a.setaId(oj.getInt("aId"));
+//					a.setDescription(oj.getString("description"));
+//					String endTime="";
+//					JSONObject end=oj.getJSONObject("");
+//					a.setEndTime();
+//				}
+//			} catch (JSONException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+		}
 		return activitys;
 	}
 
