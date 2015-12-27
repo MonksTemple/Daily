274e0fad1b81f9bc73c79567430f2d278f15debf
zbing0 <1587124918@qq.com>
2015-12-28 03:24:20 +0800

    list

--- src/com/example/presenter/ActManage.java
+++ src/com/example/presenter/ActManage.java
@@ -8,10 +8,15 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import com.alibaba.fastjson.JSON;
 import com.example.model.Activity;
 import com.example.model.User;
 import com.example.util.ConnectUtil;
+import com.example.util.DateUtil;
 import com.example.view.ActListView;
 import com.example.view.ActView;
 
@@ -61,26 +66,26 @@ public class ActManage {
 		Map<String,String> map = new HashMap<String,String>();
 		map.put("type", "26");
 		String resp = ConnectUtil.getResponse(map);
-		if(!resp.equals("false")){
-			activitys = JSON.parseArray(resp,Activity.class);
-			
-//			try {
-//				JSONObject result = new JSONObject(resp);
-//				JSONArray actList = result.getJSONArray("actList");//获取JSONArray  
-//				int size=actList.length();
-//				for(int i=0;i<size;i++){
-//					JSONObject oj = actList.getJSONObject(i);
-//					Activity a=new Activity();
-//					a.setaId(oj.getInt("aId"));
-//					a.setDescription(oj.getString("description"));
-//					String endTime="";
-//					JSONObject end=oj.getJSONObject("");
-//					a.setEndTime();
-//				}
-//			} catch (JSONException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
+		if(!resp.equals("false")){		
+			try {
+				JSONArray actList = new JSONArray(resp);
+				int size=actList.length();
+				for(int i=0;i<size;i++){
+					JSONObject oj = actList.getJSONObject(i);
+					Activity a=new Activity();
+					a.setaId(oj.getInt("aId"));
+					a.setName(oj.getString("name"));
+					a.setDescription(oj.getString("description"));
+					a.setEndTime(DateUtil.getDateFromString(oj.getString("endTime")));
+					a.setStartTime(DateUtil.getDateFromString(oj.getString("startTime")));
+					a.setRemindTime(DateUtil.getDateFromString(oj.getString("remindTime")));
+					a.setPlace(oj.getString("place"));
+					activitys.add(a);
+				}
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		return activitys;
 	}
